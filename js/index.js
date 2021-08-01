40cb5d16a29040bfd653e9f5ccc1aa7ab8fe0fc3
Ragab Hussein <ragaphussin88@gmail.com>
2021-08-01 21:07:36 +0200

    all file IS added after edeiting

--- js/index.js
+++ js/index.js
@@ -1,60 +1,47 @@
 let input_text = document.getElementById("input_text"),
-    content = document.getElementById("content"),
+    div_content = document.getElementById("content"),
     action = document.getElementById("action");
 
 action.onsubmit = function(e){
     e.preventDefault();
 } 
-    //create item content
+     //create item that within div_content
 
-function field_test(){
+function feild_test(){
 
-    // create parent content
+     // create the child
 
     let new_div = document.createElement("div"),
         new_class = document.createAttribute("class");
-        new_class.value = "content_Child";
+        new_class.value = "content_child";
 
-    // Create icon content
+        // Create icon content
 
-    let new_i = document.createElement("i"),
-        new_i_class = document.createAttribute("class");
-        new_i_class.value = "fas fa-times i_style";
-
-        new_I = document.createElement("i");
-        new_I_class = document.createAttribute("class");
-        new_I_class.value = "far fas fa-check i_style";
+    let new_icon = document.createElement("i"),
+        new_icon_class = document.createAttribute("class");
+        new_icon_class.value = "fas fa-times icon_style";
 
         // start input field
 
     if (input_text.value == ""){
-        
-        alert("you cannot leave the field blank");
-        
-    } else {
             
+        alert("you cannot leave the field blank");
+            
+    } 
+
+    else {
+    
         // Add the value of the input field
 
         new_div.setAttributeNode(new_class);
-        content.appendChild(new_div).innerHTML = input_text.value ;
+        div_content.appendChild(new_div).innerHTML = input_text.value ;
         document.getElementById("input_text").value = "";
 
-        new_i.setAttributeNode(new_i_class);
-        new_div.appendChild(new_i);
-
-        new_I.setAttributeNode(new_I_class);
-        new_div.appendChild(new_I);
-        new_I.style.display = "none"
-
-        // Unwanted field removal function
-        
-        new_i.onclick = function () {
+        new_icon.setAttributeNode(new_icon_class);
+        new_div.appendChild(new_icon);
+        new_icon.onclick = function () {
+            
             this.parentElement.style.display = 'none'; 
-        };
-
-        new_div.onclick = function () {
-            new_I.style.display = "inline-block";
         }
-
     }
 }
